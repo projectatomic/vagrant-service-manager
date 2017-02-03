2ce7643d209781d63cdc69eef2cf2c19cdee47eb
Budh Ram Gurung <budhram.gurung01@gmail.com>
2017-02-03 14:08:52 +0530

    Fix #454 fix error message for --path option

--- lib/vagrant-service-manager/installer.rb
+++ lib/vagrant-service-manager/installer.rb
@@ -45,7 +45,8 @@ module VagrantPlugins
 
         # return if --path is not specified
         return unless @options.key?('--path')
-        return if File.exist?(@options['--path'])
+        dir_name = @options['--path']
+        return if File.exist? dir_name
         @env.ui.info I18n.t('servicemanager.commands.install_cli.invalid_binary_path', dir_path: dir_name)
         exit 126
       end
